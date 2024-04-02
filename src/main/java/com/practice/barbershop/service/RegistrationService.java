260250cdfe1a7e8088bf8328263b77ac3a26522c
Dev0uver <atom.01@yandex.ru>
2024-04-02 17:14:22 +0300

    RegistrationService cleaned

--- src/main/java/com/practice/barbershop/service/RegistrationService.java
+++ src/main/java/com/practice/barbershop/service/RegistrationService.java
@@ -122,21 +122,11 @@ public class RegistrationService implements MyService<RegistrationsDto, Registra
      */
     public RegistrationsDto setClient(RegistrationsDto registrationsDto) {
         Client client = clientService.getEntityById(registrationsDto.getClient_id());
-        Barber barber = barberService.getEntityById(registrationsDto.getBarber_id());
 
         registrationsDto.setRegistrationTime(LocalDateTime.now());
         registrationsDto.setPhone(client.getPhone());
         registrationsDto.setClientName(client.getName());
         registrationsDto.setCanceled(Boolean.FALSE);
-
-//        registration.setClient(client);
-//        registration.setBarber(barber);
-//        registration.setRegistrationTime();
-//        registration.setPhone(client.getPhone());
-//        registration.setClientName(client.getName());
-//        registration.setCanceled(Boolean.FALSE);
-//        registration.setDay(date);
-//        registration.setTime(time);
 
         return registrationsDto;
     }
