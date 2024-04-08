81f3f7f08d63e1723d675168168a05f75c058689
flamebrier <flamebrier@gmail.com>
2024-04-08 14:03:28 +0300

    Add simple javadoc and clean code.

--- src/main/java/com/practice/barbershop/service/BarberService.java
+++ src/main/java/com/practice/barbershop/service/BarberService.java
@@ -35,21 +35,28 @@ public class BarberService implements MyService<BarberDto, Barber> {
     private final PhotoServiceImpl photoService;
 
     /**
-     * Return Barber entity by his id
-     * @param id entity id
+     * Get barber from db if it exists or else not found runtime exception.
+     * Change amenity price via barbers degree.
+     * Return barber dto.
+     * @param id barber id
      * @return Barber dto
      */
     @Override
     public BarberDto getDtoById(Long id) {
+
         BarberDto barber = BarberMapper.toDto(barberRepository.getBarberById(id)
                 .orElseThrow(() -> new RuntimeException("Barber with id=" + id + " not found.")));
-        barber.getAmenitiesDtoList().forEach((AmenitiesDto a) -> a.setPrice((int) Math.floor(a.getPrice() * barber.getBarberDegree().getExtraCharge())));
+
+        barber.getAmenitiesDtoList().forEach(
+                (AmenitiesDto a) ->
+                        a.setPrice((int) Math.floor(a.getPrice() * barber.getBarberDegree().getExtraCharge()))
+        );
 
         return barber;
     }
 
     /**
-     * Return Barber entity by his id
+     * Get barber from db if it exists or else not found runtime exception. Return barber entity.
      * @param id entity id
      * @return Barber entity
      */
@@ -57,12 +64,17 @@ public class BarberService implements MyService<BarberDto, Barber> {
     public Barber getEntityById(Long id) {
         return barberRepository.getBarberById(id)
                 .orElseThrow(() -> new RuntimeException("Barber with id=" + id + " not found."));
-
     }
 
     /**
-     * Save or change entity in db
-     * @param dto entity object
+     * Save entity in db. Set barber id=<code>null</code>.
+     * Save barber into db and get this new barber entity with id.
+     * Set amenity entity and photo entity lists to barber.
+     * Convert barber to dto.
+     * Change amenity price via barbers degree.
+     * Return barber dto.
+     * @param dto BarberDto object
+     * @return saved BarberDto object
      */
     @Override
     @Transactional
@@ -74,10 +86,26 @@ public class BarberService implements MyService<BarberDto, Barber> {
         savedBarber.setPhotoList(photoService.getPhotoByBarberId(savedBarber.getId()));
 
         BarberDto finalDto = BarberMapper.toDto(savedBarber);
-        finalDto.getAmenitiesDtoList().forEach((AmenitiesDto a) -> a.setPrice((int) Math.floor(a.getPrice() * dto.getBarberDegree().getExtraCharge())));
+        finalDto.getAmenitiesDtoList().forEach(
+                (AmenitiesDto a) ->
+                        a.setPrice((int) Math.floor(a.getPrice() * dto.getBarberDegree().getExtraCharge()))
+        );
         return finalDto;
     }
 
+    /**
+     * Trying to get this barber from db via id
+     * if barber not exists run new not found runtime exception.
+     * if dto amenity and photo lists != null,
+     * then convert updated barber amenity and photo entities lists
+     * to dto and set to dto object. Update barber into db.
+     * Set to updated barber amenity entity and photo entity lists.
+     * convert updated barber to dto
+     * Change amenity price via barbers degree.
+     * Return barber dto.
+     * @param dto BarberDto object
+     * @return updated BarberDto object
+     */
     @Override
     @Transactional
     public BarberDto update(BarberDto dto) {
@@ -99,17 +127,32 @@ public class BarberService implements MyService<BarberDto, Barber> {
         updatedBarber.setPhotoList(photoService.getPhotoByBarberId(updatedBarber.getId()));
 
         BarberDto finalDto = BarberMapper.toDto(updatedBarber);
-        finalDto.getAmenitiesDtoList().forEach((AmenitiesDto a) -> a.setPrice((int) Math.floor(a.getPrice() * dto.getBarberDegree().getExtraCharge())));
-
+        finalDto.getAmenitiesDtoList().forEach(
+                (AmenitiesDto a) ->
+                        a.setPrice((int) Math.floor(a.getPrice() * dto.getBarberDegree().getExtraCharge()))
+        );
         return finalDto;
     }
 
+    /**
+     * Get all Barbers from db
+     * @return List<Barber>
+     */
     @Override
     public List<BarberDto> getAllDto() {
         List<Barber> barbers = barberRepository.findAll();
         return barbers.stream().map(BarberMapper::toDto)
                 .collect(Collectors.toList());
     }
+
+    /**
+     * Add Amenity to barber via barber id and amenity id.
+     * trying to get amenity. If not found run new not found runtime exception.
+     * Then check barber amenities list not contains amenity or else run new runtime exception.
+     * If barber not contains this amenity. Add amenity to list and update barber
+     * @param barberId ID of barber
+     * @param amenityId ID of amenity
+     */
     @Transactional
     public void addAmenityToBarber(Long barberId, Long amenityId) {
         BarberDto barber = getDtoById(barberId);
@@ -129,7 +172,14 @@ public class BarberService implements MyService<BarberDto, Barber> {
             throw new RuntimeException("Amenity have already been added for this barber.");
         }
     }
-
+    /**
+     * Delete Amenity from barber via barber id and amenity id.
+     * trying to get amenity. If not found run new not found runtime exception.
+     * Then check barber amenities list contains amenity or else run new runtime exception.
+     * If barber contains this amenity. Remove amenity from list and update barber
+     * @param barberId ID of barber
+     * @param amenityId ID of amenity
+     */
     @Transactional
     public void deleteAmenityFromBarber(Long barberId, Long amenityId) {
         BarberDto barber = getDtoById(barberId);
@@ -149,6 +199,19 @@ public class BarberService implements MyService<BarberDto, Barber> {
             throw new RuntimeException("Amenity with id=" + amenityId + " not found for this barber.");
         }
     }
+
+    /**
+     * Link img to barber. Get original file name.
+     * Create new photo dto object, set barber id and file name.
+     * Trying to save converted to entity photo object.
+     * Get id of record. Make new file name via photo id and "_" and original file name.
+     * Make new path. Check folder exists. Create new file.
+     * @param path path in hard catalog.
+     * @param file Photo
+     * @param barberId Barber
+     * @return answer
+     * @throws IOException exception
+     */
     @Transactional
     public String uploadImage(String path, MultipartFile file, Long barberId) throws IOException {
         //Filename
@@ -173,6 +236,13 @@ public class BarberService implements MyService<BarberDto, Barber> {
         return name;
     }
 
+    /**
+     * Delete photo from db and local disk.
+     * @param path path in hard catalog.
+     * @param fileName Name of img
+     * @param barberId Barber id.
+     * @return Answer
+     */
     @Transactional
     public String deleteImage(String path, String fileName, Long barberId) {
         File deleteFile = new File(path + "\\" + fileName);
